d37b4666aef4546916582a0c9d65b5acb452a9db
Yossale <yossi@altostra.com>
2021-02-25 20:19:54 +0200

    2nd appointment

--- functions/2NdVaccination.js
+++ functions/2NdVaccination.js
@@ -1,5 +1,46 @@
-module.exports.handler = async (event, context) => {
-  return {
-      statusCode: 200
-  }
+const AWS = require('aws-sdk')
+
+function authenticateRequest() {
+  console.log(`Authenticating the request`)
 }
+
+function validateRequest() {
+  console.log(`Validating the request`)
+}
+
+function getNextDoseDate(firstDose) {
+  const fd = new Date(firstDose)
+
+  let nd = new Date()
+  nd.setDate(fd.getDate() + 21)
+
+  return nd.toDateString()
+}
+
+
+exports.handler = async (event) => {
+
+  authenticateRequest(event)
+
+  validateRequest(event)
+
+  let body
+
+  if (event.body) {
+    body = JSON.parse(event.body)
+  } else {
+    return {
+      statusCode: 400,
+      body: `Bad Request, body not present`,
+    }
+  }
+
+  console.log(`Scheduing next dose appointment for user: ${body.userId}`)
+
+  const nextDoseDate = getNextDoseDate(body.firstDoseDate)
+
+  return {
+    statusCode: 200,
+    body: `${nextDoseDate}`,
+  };
+};
